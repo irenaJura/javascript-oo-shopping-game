0210997c994bdaefd8e8277020daead7f2b558d8
Irena Jurasek <irenaskydive@gmail.com>
2020-08-16 16:57:25 +0200

    Calculate total bill & points for expiry date

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -257,7 +257,9 @@ const getProduct = (prodList, pId) => {
 };
 
 // Complete this function
-const calculateBill = (prod, tBill) => {};
+const calculateBill = (prod, tBill) => {
+  return tBill + prod.price;
+};
 
 const findPointsToBill = (roundedTotal) => {
   if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -278,7 +280,13 @@ const findPointsToBill = (roundedTotal) => {
 };
 
 // Complete this function
-const findPointsForExpDate = (prod) => {};
+const findPointsForExpDate = (prod) => {
+  if (prod.daysToExpire < 30) {
+    return 10;
+  } else {
+    return 0;
+  }
+};
 
 const calculatePoints = (prod, tBill) => {
   let pointsToBill = findPointsToBill(Math.round(tBill));
