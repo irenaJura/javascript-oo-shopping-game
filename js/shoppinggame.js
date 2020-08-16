df7007d5b7228b083af88e3344dfe1730df1bb42
Irena Jurasek <irenaskydive@gmail.com>
2020-08-16 11:26:38 +0200

    Add getDetails method to Product

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -40,7 +40,9 @@ Object.defineProperty(Product.prototype, "daysToExpire", {
   },
 });
 // Add method getDetails to Product here
-
+Product.prototype.getDetails = function () {
+  return `Product Name: ${this.name} , Product Price: ${this.price}`;
+};
 // Define the MagicProduct class here
 
 // Establish inheritance between Product() & MagicProduct() here
